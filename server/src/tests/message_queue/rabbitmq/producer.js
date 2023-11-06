c87d7a557265d88342b1af56b5262617cefe2a2b
phanquangduong2002 <phanquangduong2002@gmail.com>
2023-11-06 18:02:53 +0700

    commit

---
+++ server/src/tests/message_queue/rabbitmq/producer.js
@@ -0,0 +1,24 @@
+const amqp = require("amqplib");
+
+const message = "Hello, RabbitMQ";
+
+const runProducer = async () => {
+  try {
+    const connection = await amqp.connect("amqp://guest:12345@localhost");
+    const channel = await connection.createChannel();
+
+    const queueName = "test-topic";
+
+    await channel.assertQueue(queueName, {
+      durable: true,
+    });
+
+    // send message to consumer
+    channel.sendToQueue(queueName, Buffer.from(message));
+    console.log(`message send:`, message);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+runProducer().catch(console.error);
